c70048369bdd2bc584fe36e6b2ff82cd9f60c7da
noavarice <noavarice@gmail.com>
2025-05-16 11:51:14 +0300

    refactor: fix indents, use JSpecify

--- src/main/java/com/github/build/Dependency.java
+++ src/main/java/com/github/build/Dependency.java
@@ -1,5 +1,11 @@
 package com.github.build;
 
+/**
+ * Project dependency.
+ *
+ * @author noavarice
+ * @since 1.0.0
+ */
 public interface Dependency {
 
 }
--- src/main/java/com/github/build/Project.java
+++ src/main/java/com/github/build/Project.java
@@ -1,11 +1,44 @@
 package com.github.build;
 
-import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
+/**
+ * Project to build.
+ *
+ * @author noavarice
+ * @since 1.0.0
+ */
 public interface Project {
 
-    String id();
+  /**
+   * Identifies project, so you don't need to mess with physical project location on disk.
+   *
+   * @return Project ID, never null
+   */
+  Id id();
 
-    Set<Dependency> dependencies();
+  /**
+   * Project dependencies.
+   *
+   * @return Unmodifiable set of project dependencies, never null but can be empty
+   */
+  Set<Dependency> dependencies();
+
+  /**
+   * Project ID.
+   *
+   * @param value ID string
+   */
+  record Id(String value) {
+
+    public Id {
+      Objects.requireNonNull(value);
+      if (value.isBlank()) {
+        throw new IllegalArgumentException("Must not be empty");
+      }
+
+      value = value.strip();
+    }
+  }
 }
