7503c5caca6c56b13c92e1604a98d764c771d8f6
noavarice <noavarice@gmail.com>
2025-08-26 15:15:54 +0300

    test: add dependency resolution test

--- lib/src/test/java/com/github/build/DependencyServiceTest.java
+++ lib/src/test/java/com/github/build/DependencyServiceTest.java
@@ -32,12 +32,11 @@ class DependencyServiceTest {
       "1.5.18"
   );
 
-  private final Dependency.Remote.Exact springCore = new Dependency.Remote.Exact(
+  private final Dependency.Remote.Exact springContext = new Dependency.Remote.Exact(
       "org.springframework",
-      "spring-core",
+      "spring-context",
       "6.0.11"
   );
-
 
   private final RemoteRepository mavenCentral = new RemoteRepository(
       URI.create("https://repo.maven.apache.org/maven2"),
@@ -51,13 +50,33 @@ class DependencyServiceTest {
   void testResolvingTransitiveDependencies() {
     final var ref = new AtomicReference<Set<Coordinates>>();
     assertThatCode(
-        () -> ref.set(service.resolveTransitive(springCore))
+        () -> ref.set(service.resolveTransitive(logbackClassic))
     ).doesNotThrowAnyException();
 
     final Set<Coordinates> actual = ref.get();
     final Set<Coordinates> expected = Set.of(
         new Coordinates("ch.qos.logback", "logback-core", "1.5.18"),
         new Coordinates("org.slf4j", "slf4j-api", "2.0.17")
+    );
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @DisplayName("Check resolving more transitive dependencies for a single dependency")
+  @Test
+  void testResolvingMoreTransitiveDependencies() {
+    final var ref = new AtomicReference<Set<Coordinates>>();
+    assertThatCode(
+        () -> ref.set(service.resolveTransitive(springContext))
+    ).doesNotThrowAnyException();
+
+    final Set<Coordinates> actual = ref.get();
+    final Set<Coordinates> expected = Set.of(
+        new Coordinates("org.springframework", "spring-aop", "6.0.11"),
+        new Coordinates("org.springframework", "spring-beans", "6.0.11"),
+        new Coordinates("org.springframework", "spring-core", "6.0.11"),
+        new Coordinates("org.springframework", "spring-expression", "6.0.11"),
+        new Coordinates("org.springframework", "spring-jcl", "6.0.11")
     );
 
     assertThat(actual).isEqualTo(expected);
