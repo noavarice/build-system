353bb6282212901975c9f794e5dada4faeab22ad
noavarice <noavarice@gmail.com>
2026-09-19 18:04:24 +0300

    Setup Java module

---
+++ lib/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author noavarice
+ * @since 1.0.0
+ */
+module com.github.build {
+  requires org.jspecify;
+  requires org.slf4j;
+  requires java.compiler;
+  requires maven.artifact;
+  requires jakarta.xml.bind;
+  requires java.net.http;
+  requires maven.model;
+  requires plexus.utils;
+  requires org.apache.maven.resolver;
+}
